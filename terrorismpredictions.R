8c22cf8ba3c3dca8d2b8841ed665d7a6226a1d54
roosevelter <lc1976@nyu.edu>
2015-05-17 11:02:36 -0400

    Update terrorismpredictions.R

--- terrorismpredictions.R
+++ terrorismpredictions.R
@@ -1,4 +1,5 @@
-library(caret); library(C50); library(e1070)
+library(caret); library(C50); library(e1070); library(randomForest); library(class); library(ipred)
+data = na.omit(data)
 set.seed(1993)
 dataIndex = createDataPartition(data$terroristattack, p = .75, list = F, times = 1)
 dataTrain = data[dataIndex,]
@@ -20,3 +21,28 @@ data = data[,-c(1:2,4)]
 data$terroristattack = as.factor(data$terroristattack)
 set.seed(1993)
 m = train(terroristattack ~ ., data = data, method = "C5.0")
+
+set.seed(1993)
+rf1 = randomForest(terroristattack ~ ., data = data)
+
+ctrl = trainControl(method = 'repeatedcv', number = 10, repeats = 10)
+grid_rf = expand.grid(.mtry = c(2,4,8,16))
+set.seed(1993)
+rf2 = train(terroristattack ~ ., data = data, method = 'rf', metric = 'Accuracy', trControl = ctrl, tuneGrid = grid_rf)
+
+grid_c50 <- expand.grid(.model = "tree", .trials = c(10, 20, 30, 40), .winnow = "FALSE")
+set.seed(1993)
+m50 = train(terroristattack ~ ., data=data, method="C5.0", metric = "Accuracy", trControl = ctrl, tuneGrid = grid_c50)
+
+normalize = function(x) ((x-min(x))/(max(x)- min(x)))
+knnData = as.data.frame(lapply(data[,2:ncol(data)], normalize))
+knnPredict = knn(train = dataTrain, test = dataTest, cl = dataTrain[,1], k = 9)
+CrossTable(x = dataTest[,1], y = knnPredict, prop.chisq = F)
+
+set.seed(1993)
+mybag = bagging(terroristattack ~ ., data=data, nbagg=25)
+bag_pred = predict(mybag, data)
+table(bag_pred, data$terroristattack)
+set.seed(1993)
+ctrl = trainControl(method = "cv", number = 10)
+train(terroristattack ~ ., data = data, method = 'treebag', trControl = ctrl)
